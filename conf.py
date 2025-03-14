e6b84f05f252496ce0bd128729b9ba6c148904c6
Arun Persaud <apersaud@lbl.gov>
2025-03-13 22:46:27 -0700

    Make images work (needs to have downloaded full resolution to /images and /galleries, curently not available)

--- conf.py
+++ conf.py
@@ -145,7 +145,7 @@ NAVIGATION_LINKS = {
         ("/", "Home"),
         ("/meetup", "Weekly Meeting"),
         ("/news", "News and Events"),
-        ("/media", "Fotos & Videos"),
+        ("/galleries", "Fotos & Videos"),
         ("/patterns", "Juggling Patterns"),
         ("https://berkeleyunicycling.org", "Unicycling"),
         ("/links", "Links"),
@@ -784,7 +784,7 @@ GITHUB_COMMIT_SOURCE = True
 # "source/" and the results will be located in
 # "OUTPUT_PATH/relative_destination/gallery_name"
 # Default is:
-GALLERY_FOLDERS = {"galleries": "media"}
+GALLERY_FOLDERS = {"galleries": "galleries"}
 # More gallery options:
 # THUMBNAIL_SIZE = 180
 # MAX_IMAGE_SIZE = 1280
