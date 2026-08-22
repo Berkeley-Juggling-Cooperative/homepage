de3afdbc5b4cb8580d3b73c38ad4469a7028ad9a
Arun Persaud <apersaud@lbl.gov>
2026-08-22 12:39:13 -0700

    fix: lightbox was not loaded

--- themes/mytheme/assets/js/gallery-init.js
+++ themes/mytheme/assets/js/gallery-init.js
@@ -9,9 +9,27 @@
             var jsonContent = JSON.parse(photoArrayAttr);
             var thumbnailSize = parseInt(thumbnailSizeAttr, 10);
 
+            // site.js runs baguetteBox.run() when all-nocdn.js loads, which is
+            // before these thumbnails exist -- and renderGallery() replaces them
+            // again on every resize.  Re-bind after each render or clicking a
+            // thumbnail just navigates to the bare image.
+            var bindLightbox = function() {
+                if (typeof baguetteBox !== 'undefined') {
+                    baguetteBox.run('div#content', {
+                        ignoreClass: 'islink',
+                        captions: function(element){
+                            var i = element.getElementsByTagName('img')[0];
+                            return i === undefined ? '' : i.alt;
+                        }
+                    });
+                }
+            };
+
             renderGallery(jsonContent, thumbnailSize);
+            bindLightbox();
             window.addEventListener('resize', function(){
                 renderGallery(jsonContent, thumbnailSize);
+                bindLightbox();
             });
         }
     }
